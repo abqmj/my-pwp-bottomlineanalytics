6a75939ab4b3491a12acf99598cb5ada1d4a878b
Michael Jordan <mikesjordan@gmail.com>
2017-04-26 19:20:20 -0600

    added lang=en to html per the validator site.. It warned me to added the content width tag etcetc... but i've already added that so idk why that shows as an error anyways everything else is done god willing

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -1,11 +1,11 @@
 <!DOCTYPE HTML>
-<html>
+<html lang="en">
 	<head>
 		<meta charset="UTF-8">
 		<meta name="viewport"
 				content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
 		<meta http-equiv="X-UA-Compatible" content="ie=edge">
-		<title>Bottom line Analytics</title>
+		<title>Bottom line Analytics US</title>
 	</head>
 	<body>
 		<h1>Bottom Line Analytics purpose</h1>
@@ -14,7 +14,23 @@
 			<p>Also provided is training on cash flow, accounting standards, and appraisal standards.</p>
 		<h2>Bottom Line Analytics audience</h2>
 		<p>The Company's target market is financial institutions that lend funds to business or leasing companies that lease equipment to businesses.</p>
-		<h3>Bottom Line Analytics goal</h3>
+		<h2>Bottom Line Analytics goal</h2>
 		<p>With our new website we hope to attract to new clients and market to existing clients, as well as provide easily accessible contact information.</p>
+		<h3>Persona</h3>
+		<p><strong>NAME: </strong>Bob Dobalina
+		<br>
+		<strong>AGE: </strong>38</p>
+		<p><strong>Profession: </strong> Restaurant General Manager for over 20 years. Also small business owner making homemade salsa, which he sells at local farmers markets. Looking to invest his savings into his sports bar with a unique menu built around his homemade salsa.</p>
+		<p><strong>Technology: </strong> Windows Vista user when at work because corporate is afraid to retrain the older managers, Iphone 7 with verizon, and uses a dell gaming laptop at home with 1050ti vc i5 proc. </p>
+		<p><strong>Attitudes and Behaviors: </strong> Extremely friendly and always puts the customer first which is why he's lasted 20 years in the service industry. Being a general manager means he never really has a day off, but when he is off of work he spends that time cooking his homemade salsa.</p>
+		<p><strong>Frustrations and Needs: </strong>Bob has worked for a corporate chain for over 20 years and is ready to use everything he has learned in that time to grow his own sports bar. He needs a loan of 350k to purchase kitchen equipment, 6 months of estimated payroll until the business grows enough to cover, and at least 2 years of rent for his location.</p>
+		<p><strong>Goals: </strong>Bobs number one goal for his sports bar is to not end up on Bar Rescue because John Tahfer is a dick.</p>
+		<p><strong>User Story: </strong> Bobs hopes his sports bar will shake up the scene in his local town and provide sports fan with real authentic food.</p>
+		<H3>USE CASE</H3>
+		<p>Bob was at his local bank in hopes of being financed for a loan to start his business. However he did not meet the criteria that the bank requires for said loan.</p>
+		<p>So Bob called a friend of his who mentioned knowing someone who helps start up businesses get loans. Bob calls his friend but his friend was unable to find the business card. Fortunately Bob's friend remembered the website BottomLineAnalyticsUS.com and Bob was able to easily view the sites contact page on his phone.</p>
+		<p>Once Bob got home he visited the website again and called the number listed on BottomLineAnalyticsUS.com's contact page and set up a meeting with the owner Myra Jordan.</p>
+		<p>Bob met with Myra and after paying her fee she went over all of his financicals with him. Bob was then able to get his loan and make his dream of being the owner of his own sports bar a reality.</p>
+		<p>Myra's awesome brother-in-law who set up the website got a cut of her fee. YAAAAAAAAAY</p>
 	</body>
 </html>
