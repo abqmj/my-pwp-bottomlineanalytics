50a0580fa963a9d4549beb07bd0777a15776a453
Michael Jordan <mikesjordan@gmail.com>
2017-04-26 18:32:57 -0600

    purpose audience and goal of bottom line analytics added

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -8,6 +8,13 @@
 		<title>Bottom line Analytics</title>
 	</head>
 	<body>
-		stuff goes here
+		<h1>Bottom Line Analytics purpose</h1>
+		<p>We provide independent written reviews of loan requests which includes a summary assessment of the 5 C's of credit, decision recommendations, observed strengths and weaknesses, and recommended conditions of funding should the client choose to approve the loan.</p>
+		<p>The company also provides internal real estate evaluations that can be used by financial institutions in situations whereby appraisals are not required.</p>
+			<p>Also provided is training on cash flow, accounting standards, and appraisal standards.</p>
+		<h2>Bottom Line Analytics audience</h2>
+		<p>The Company's target market is financial institutions that lend funds to business or leasing companies that lease equipment to businesses.</p>
+		<h3>Bottom Line Analytics goal</h3>
+		<p>With our new website we hope to attract to new clients and market to existing clients, as well as provide easily accessible contact information.</p>
 	</body>
 </html>
