b207caa2050fc298bae247387cf1ac2249bb965e
Michael Jordan <mikesjordan@gmail.com>
2017-05-01 13:49:19 -0600

    work on portfolio this is just what myra gave me

--- public_html/documentation/milestone-2.php
+++ public_html/documentation/milestone-2.php
@@ -12,7 +12,14 @@
 			<strong>Welcome: </strong> Hi my name is Myra Jordan I provide independent written reviews of loan requests which includes a summary assessment of the 5 C's of credit, decision recommendations, observed strengths and weaknesses, and recommended conditions of funding should the client choose to approve the loan.
 		</p>
 		<p>
-			<strong>About Me: </strong>
+			<strong>About Me: </strong>Bottom Line Analytics provides commercial credit underwriting services to commercial finance organizations across the US, specializing in Commercial Real Estate Investment, Construction and C&I analysis.
+			Myra Jordan, CRC, Founder, has been in the banking and micro-finance industries for nearly 10 years. She understands that each organization has it's own risk tolerance, and uses her expertise to assess the 5 C's of credit and determine if the loan facility is within an organizations risk tolerance.
+		</p>
+		<p>
+			<strong>Portfolio: </strong>I want the website to include a "home page" and a tools page with an amortization schedule, 5 C's of Credit explanation and a Credit Dictionalry (I have all these), and a Sources page with links to SBA, Accion, BLA, GAAR, CBRE. But do whatever you want, really.
+		</p>
+		<p>
+			<strong>Contact: </strong>Myra Jordan, CRC, 505-440-8068, email mjordanbottomlineanalytics@gmail.com
 		</p>
 	</body>
 </html>
