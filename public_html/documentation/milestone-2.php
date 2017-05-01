f19c84ff4a36b02c395063b6cdad6095ee856dad
Michael Jordan <mikesjordan@gmail.com>
2017-05-01 11:52:25 -0600

    started milestone-2

---
+++ public_html/documentation/milestone-2.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<meta name="viewport"
+				content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+		<meta http-equiv="X-UA-Compatible" content="ie=edge">
+		<title>PWP milestone-2 content strategy</title>
+	</head>
+	<body>
+		<p>
+			<strong>Welcome: </strong> Hi my name is Myra Jordan I provide independent written reviews of loan requests which includes a summary assessment of the 5 C's of credit, decision recommendations, observed strengths and weaknesses, and recommended conditions of funding should the client choose to approve the loan.
+		</p>
+		<p>
+			<strong>About Me: </strong>
+		</p>
+	</body>
+</html>
