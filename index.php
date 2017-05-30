f607a6fa984a377ccbcebf8481f0da3e686428a8
Michael Jordan <mikesjordan@gmail.com>
2017-05-29 19:22:14 -0600

    showing myra

---
+++ index.php
@@ -0,0 +1,174 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+
+		<!-- Latest compiled and minified CSS -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+		<!-- Optional theme -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+		<!-- Font Awesome -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+
+		<!-- My Custom CSS -->
+		<link rel="stylesheet" href="css/style.css" type="text/css">
+
+		<!-- HTML5 shiv and Respond.js for IE8 support of HTML5 elements and media queries -->
+		<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+		<!--[if lt IE 9]>
+		<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+		<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+		<![endif]-->
+
+		<!-- jQuery v3.0 -->
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.0.0/jquery.min.js" type="text/javascript"></script>
+
+		<!-- Latest compiled and minified JavaScript -->
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+
+		<title>Bootstrap Challenge Demo</title>
+	</head>
+	<body class="sfooter">
+		<div class="sfooter-content">
+
+			<header>
+				<nav class="navbar navbar-default">
+					<div class="container">
+						<!-- Brand and toggle get grouped for better mobile display -->
+						<div class="navbar-header">
+							<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#main-menu" aria-expanded="false">
+								<span class="sr-only">Toggle navigation</span>
+								<span class="icon-bar"></span>
+								<span class="icon-bar"></span>
+								<span class="icon-bar"></span>
+							</button>
+							<a class="navbar-brand" href="#">Bootstrap Challenge Demo</a>
+						</div>
+
+						<!-- Collect the nav links, forms, and other content for toggling -->
+						<div class="collapse navbar-collapse" id="main-menu">
+							<ul class="nav navbar-nav navbar-right">
+								<li><a href="https://github.com/rlewis2892/bootstrap-challenge-demo" target="_blank">GitHub</a></li>
+								<li><a href="https://bootcamp-coders.cnm.edu/~rlewis37" target="_blank">RL Deployment</a></li>
+								<li><a href="https://angular.io/" target="_blank">Angular.io</a></li>
+							</ul>
+						</div><!-- /.navbar-collapse -->
+					</div><!-- /.container-fluid -->
+				</nav>
+			</header>
+
+			<!-- Welcome Section -->
+			<section>
+				<div class="container">
+					<div class="row">
+						<div class="col-md-12">
+							<div class="jumbotron text-center">
+								<h1>Welcome to the Bootstrap Challenge</h1>
+								<p>La croix chambray organic ugh, plaid raw denim pok pok umami gastropub neutra deep v paleo jianbing semiotics cray.</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</section>
+
+			<!-- Photo and Text Section -->
+			<section class="bg-info">
+				<div class="container">
+					<div class="row">
+						<div class="col-md-3">
+							<img class="img-responsive img-thumbnail" src="https://www.fillmurray.com/g/500/300" alt="bill murray">
+						</div>
+						<div class="col-md-9">
+							<p>Church-key 3 wolf moon disrupt cronut organic, sriracha narwhal meggings. Whatever ethical chicharrones tilde, taxidermy ennui XOXO 3 wolf moon sustainable keytar crucifix franzen vexillologist seitan viral. Slow-carb taxidermy drinking vinegar authentic, woke butcher plaid banjo austin. Neutra la croix flannel meditation knausgaard flexitarian. Craft beer franzen gochujang marfa chia, direct trade squid gastropub four dollar toast everyday carry. Gentrify vice hexagon, cornhole seitan four dollar toast put a bird on it post-ironic celiac. Dreamcatcher sriracha pop-up subway tile, PBR&B tumblr health goth williamsburg pinterest microdosing photo booth keffiyeh.</p>
+						</div>
+					</div>
+				</div>
+			</section>
+
+			<!-- Thumbnail Section -->
+			<section>
+				<div class="container">
+					<div class="row">
+						<div class="col-md-3 text-center">
+							<!-- icon -->
+							<span class="fa-stack fa-5x">
+								<i class="fa fa-circle fa-stack-2x"></i>
+								<i class="fa fa-beer fa-stack-1x fa-inverse"></i>
+							</span>
+
+							<!-- panel -->
+							<div class="panel panel-default">
+								<div class="panel-body">
+									<h3>Humblebrag</h3>
+									<p>Selvage typewriter forage, craft beer vape glossier gastropub 3 wolf moon intelligentsia.</p>
+								</div>
+							</div>
+						</div><!--/.col-md-3 -->
+
+						<div class="col-md-3 text-center">
+							<!-- icon -->
+							<span class="fa-stack fa-5x">
+								<i class="fa fa-circle fa-stack-2x"></i>
+								<i class="fa fa-beer fa-stack-1x fa-inverse"></i>
+							</span>
+
+							<!-- panel -->
+							<div class="panel panel-default">
+								<div class="panel-body">
+									<h3>Humblebrag</h3>
+									<p>Selvage typewriter forage, craft beer vape glossier gastropub 3 wolf moon intelligentsia.</p>
+								</div>
+							</div>
+						</div><!--/.col-md-3 -->
+
+						<div class="col-md-3 text-center">
+							<!-- icon -->
+							<span class="fa-stack fa-5x">
+								<i class="fa fa-circle fa-stack-2x"></i>
+								<i class="fa fa-beer fa-stack-1x fa-inverse"></i>
+							</span>
+
+							<!-- panel -->
+							<div class="panel panel-default">
+								<div class="panel-body">
+									<h3>Humblebrag</h3>
+									<p>Selvage typewriter forage, craft beer vape glossier gastropub 3 wolf moon intelligentsia.</p>
+								</div>
+							</div>
+						</div><!--/.col-md-3 -->
+
+						<div class="col-md-3 text-center">
+							<!-- icon -->
+							<span class="fa-stack fa-5x">
+								<i class="fa fa-circle fa-stack-2x"></i>
+								<i class="fa fa-beer fa-stack-1x fa-inverse"></i>
+							</span>
+
+							<!-- panel -->
+							<div class="panel panel-default">
+								<div class="panel-body">
+									<h3>Humblebrag</h3>
+									<p>Selvage typewriter forage, craft beer vape glossier gastropub 3 wolf moon intelligentsia.</p>
+								</div>
+							</div>
+						</div><!--/.col-md-3 -->
+					</div><!--/.row-->
+				</div><!--/.container-->
+			</section>
+
+		</div>
+		<footer class="bg-info text-center">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-4 col-md-offset-4">
+						footer text
+					</div>
+				</div>
+			</div>
+		</footer>
+	</body>
+</html>
