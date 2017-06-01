592beaca75c7b4b642e60384457b543ac91a9d50
Michael Jordan <mikesjordan@gmail.com>
2017-06-01 15:13:18 -0600

    ignored stuff fixed css on mail form

--- public_html/index.php
+++ public_html/index.php
@@ -33,6 +33,16 @@
 		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"
 				  integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa"
 				  crossorigin="anonymous"></script>
+		<!-- jQuery Form, Additional Methods, Validate -->
+		<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery.form/4.2.1/jquery.form.min.js"></script>
+		<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.16.0/jquery.validate.min.js"></script>
+		<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.16.0/additional-methods.min.js"></script>
+
+		<!-- Your JavaScript Form Validator -->
+		<script src="js/form-validate.js"></script>
+
+		<!-- Google reCAPTCHA -->
+		<script src='https://www.google.com/recaptcha/api.js'></script>
 		<title>Bottom Line Analytics</title>
 	</head>
 	<body class="sfooter">
@@ -185,62 +195,62 @@
 																		</div>
 																	</div>
 																</div><!--/.col-md-3 -->
-																<!-- contact form -->
+																<!-- BEGIN CONTACT FORM -->
 																<div class="row">
-																	<h1 id="headers-ContactForm">Contact Bottomline Analytics here!</h1>
 																	<div class="col-md-6"></div>
 																	<div class="col-md-12 center-block">
-																		<form class="form-horizontal well" action="email.php">
-																			<div class="form-group">
-																				<label for="name">Name</label>
-																				<div class="input-group">
-																					<div class="input-group-addon">
-																						<i class="fa fa-user" aria-hidden="true"></i>
-																					</div>
-																					<input type="text" class="form-control" id="name"
-																							 name="name" placeholder="Name">
-																				</div>
+																<form id="contact-form"  class="form-horizontal well" method="POST" action="php/mailer.php">
+																	<div class="form-group">
+																		<label for="name">Name</label>
+																		<div class="input-group">
+																			<div class="input-group-addon">
+																				<i class="fa fa-user"></i>
 																			</div>
-																			<div class="form-group">
-																				<label for="email">Email address</label>
-																				<div class="input-group">
-																					<div class="input-group-addon">
-																						<i class="fa fa-envelope" aria-hidden="true"></i>
-																					</div>
-																					<input type="email" class="form-control" id="email"
-																							 name="email" placeholder="Email">
-																				</div>
-																			</div>
-																			<div class="form-group">
-																				<label for="subject">Subject</label>
-																				<div class="input-group">
-																					<div class="input-group-addon">
-																						<i class="fa fa-pencil" aria-hidden="true"></i>
-																					</div>
-																					<input type="text" class="form-control" id="subject"
-																							 name="subject" placeholder="Subject">
-																				</div>
-																			</div>
-																			<div class="form-group">
-																				<label for="message">Message</label>
-																				<div class="input-group">
-																					<div class="input-group-addon">
-																						<i class="fa fa-comment" aria-hidden="true"></i>
-																					</div>
-																					<textarea class="form-control" rows="5" id="message"
-																								 name="message"
-																								 placeholder="Message"></textarea>
-																				</div>
-																			</div>
-																			<button class="btn btn-success" type="submit"><i
-																					class="fa fa-paper-plane"></i> Send
-																			</button>
-																			<button class="btn btn-warning" type="reset"><i
-																					class="fa fa-ban"></i> Reset
-																			</button>
-																		</form>
+																			<input class="form-control" type="text" name="name" id="name" placeholder="Your Name">
+																		</div>
 																	</div>
-																</div>
+																	<div class="form-group">
+																		<label for="email">Email</label>
+																		<div class="input-group">
+																			<div class="input-group-addon">
+																				<i class="fa fa-envelope"></i>
+																			</div>
+																			<input class="form-control" type="email" name="email" id="email" placeholder="Your Email">
+																		</div>
+																	</div>
+																	<div class="form-group">
+																		<label for="subject">Subject</label>
+																		<div class="input-group">
+																			<div class="input-group-addon">
+																				<i class="fa fa-pencil"></i>
+																			</div>
+																			<input class="form-control" type="text" name="subject" id="subject" placeholder="Subject">
+																		</div>
+																	</div>
+																	<div class="form-group">
+																		<label for="message">Message</label>
+																		<div class="input-group">
+																			<div class="input-group-addon">
+																				<i class="fa fa-comment"></i>
+																			</div>
+																			<textarea rows="5" name="message" id="message" class="form-control" placeholder="Your Message (2000 charaters max)"></textarea>
+																		</div>
+																	</div>
+																	<div class="g-recaptcha" data-sitekey="6Lf0zyMUAAAAAHIP9lcPqLz8Ze2C7hxQP12tQQtF"></div>
+																	<button class="btn btn-default" type="reset">Reset</button>
+																	<button class="btn btn-info" type="submit">Submit</button>
+																</form>
+																<!-- END CONTACT FORM-->
+
+															</div><!-- /.col-sm-6 -->
+														</div><!-- /.row -->
+
+														<!-- EMPTY FORM OUTPUT AREA -->
+														<div class="row">
+															<div class="col-md-6">
+																<div id="output-area"></div>
+															</div>
+														</div>
 																<div class="row">
 																	<div class="col-xs-12">
 																		<div id="output-area"></div>
