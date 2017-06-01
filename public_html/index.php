42db9f2b422c8df86a2e1eb6fe035840e8c8b8bf
Michael Jordan <mikesjordan@gmail.com>
2017-06-01 13:21:15 -0600

    added composer.json

--- public_html/index.php
+++ public_html/index.php
@@ -2,14 +2,16 @@
 <html>
 	<head>
 		<meta charset="utf-8">
-		<meta http-equiv="X-UA-Compatible" content="IE=edge" />
-		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+		<meta name="viewport" content="width=device-width, initial-scale=1"/>
 
 		<!-- Latest compiled and minified CSS -->
-		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
+				integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 
 		<!-- Optional theme -->
-		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css"
+				integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
 
 		<!-- Font Awesome -->
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
@@ -28,8 +30,9 @@
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.0.0/jquery.min.js" type="text/javascript"></script>
 
 		<!-- Latest compiled and minified JavaScript -->
-		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
-
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"
+				  integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa"
+				  crossorigin="anonymous"></script>
 		<title>Bottom Line Analytics</title>
 	</head>
 	<body class="sfooter">
@@ -40,7 +43,8 @@
 					<div class="container">
 						<!-- Brand and toggle get grouped for better mobile display -->
 						<div class="navbar-header">
-							<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#main-menu" aria-expanded="false">
+							<button type="button" class="navbar-toggle collapsed" data-toggle="collapse"
+									  data-target="#main-menu" aria-expanded="false">
 								<span class="sr-only">Toggle navigation</span>
 								<span class="icon-bar"></span>
 								<span class="icon-bar"></span>
@@ -57,7 +61,7 @@
 								<li><a id="nav-ul" href="#headers-Portfolio">Portfolio</a></li>
 								<li><a id="nav-ul" href="#headers-Services">Services</a></li>
 								<li><a id="nav-ul" href="#headers-Testimonials">Testimonials</a></li>
-								<li><a id="nav-ul" href="#ContactForm">Contact</a></li>
+								<li><a id="nav-ul" href="#headers-ContactForm">Contact</a></li>
 							</ul>
 						</div><!-- /.navbar-collapse -->
 					</div><!-- /.container-fluid -->
@@ -76,11 +80,15 @@
 			</section>
 			<div class="bg-2 text-center">
 				<div class="container-fluid">
-			<!--About Me -->
-			<h1 id="headers-AboutMe">About Bottomline Analytics</h1>
-			<div class="aboutMe">
-				<p class="aboutMeText">Bottomline Analytics provides commercial credit underwriting services to commercial finance organizations across the US. Myra Jordan, CRC, Founder, has been in the banking and micro-finance industries for nearly 10 years. She understands that each organization has it's own risk tolerance, and uses her expertise to assess the 5 C's of credit and determine if the loan facility is within an organizations risk tolerance.</p>
-			</div>
+					<!--About Me -->
+					<h1 id="headers-AboutMe">About Bottomline Analytics</h1>
+					<div class="aboutMe">
+						<p class="aboutMeText">Bottomline Analytics provides commercial credit underwriting services to
+							commercial finance organizations across the US. Myra Jordan, CRC, Founder, has been in the banking
+							and micro-finance industries for nearly 10 years. She understands that each organization has it's
+							own risk tolerance, and uses her expertise to assess the 5 C's of credit and determine if the loan
+							facility is within an organizations risk tolerance.</p>
+					</div>
 				</div>
 			</div>
 			<div class="bg-2 text-center">
@@ -88,7 +96,9 @@
 					<!--Portfolio -->
 					<h1 id="headers-Portfolio">Portfolio</h1>
 					<div class="Portfolio">
-						<p class="portfolioText">5 C's of Credit explanation and a Credit Dictionalry (I have all these), and a Sources page with links to SBA, Accion, BLA, GAAR, CBRE. HI ROCHELLE I'm gonna sit down with myra asap and go over what she actually wants here.</p>
+						<p class="portfolioText">5 C's of Credit explanation and a Credit Dictionalry (I have all these), and
+							a Sources page with links to SBA, Accion, BLA, GAAR, CBRE. HI ROCHELLE I'm gonna sit down with myra
+							asap and go over what she actually wants here.</p>
 					</div>
 				</div>
 			</div>
@@ -97,129 +107,153 @@
 					<!--Services -->
 					<h1 id="headers-Services">Services</h1>
 					<div class="Services">
-						<p class="servicesText">Small business underwriting, including RE Investment, Construction and C&I analysis.
-							Small business underwriting/lending trainings and consulting including Underwriting best practices, development of risk tolerance and Underwriting policies.  </p>
+						<p class="servicesText">Small business underwriting, including RE Investment, Construction and C&I
+							analysis.
+							Small business underwriting/lending trainings and consulting including Underwriting best practices,
+							development of risk tolerance and Underwriting policies. </p>
 					</div>
 				</div>
 			</div>
 			<!-- Testimonials -->
 			<section>
 				<div class="bg-2 text-center">
-				<h1 id="headers-Testimonials">Testimonials</h1>
-				<div class="container">
-					<div class="row">
-						<div class="col-md-3 text-center">
+					<h1 id="headers-Testimonials">Testimonials</h1>
+					<div class="container">
+						<div class="row">
+							<div class="col-md-3 text-center">
 
-							<!-- Profile Pic-->
-									<img src="images/bobross.jpg" alt="doge" class="img-circle" id="myPhoto">
-							<!-- Testimonials -->
-							<div class="panel panel-default">
-								<div class="panel-body">
-									<p id="panel-bodyText">Myra is amazing.Myra is amazing.Myra is amazing.Myra is amazing.Myra is amazing.Myra is amazing.Myra is amazing.Myra is amazing.Myra is amazing.Myra is amazing.Myra is amazing.Myra is amazing.Myra is amazing.</p>
+								<!-- Profile Pic-->
+								<img src="images/bobross.jpg" alt="doge" class="img-circle" id="myPhoto">
+								<!-- Testimonials -->
+								<div class="panel panel-default">
+									<div class="panel-body">
+										<p id="panel-bodyText">Myra is amazing.Myra is amazing.Myra is amazing.Myra is
+											amazing.Myra is amazing.Myra is amazing.Myra is amazing.Myra is amazing.Myra is
+											amazing.Myra is amazing.Myra is amazing.Myra is amazing.Myra is amazing.</p>
+									</div>
 								</div>
-							</div>
-						</div><!--/.col-md-3 -->
-						<!-- Testimonials -->
-						<section>
-							<div class="container">
-								<div class="row">
-									<div class="col-md-3 text-center">
-										<!-- Profile Pic-->
-										<img src="images/bobross.jpg" alt="doge" class="img-circle" id="myPhoto">
-										<!-- Testimonials -->
-										<div class="panel panel-default">
-											<div class="panel-body">
-												<p id="panel-bodyText">Myra is amazing.Myra is amazing.Myra is amazing.Myra is amazing.Myra is amazing.Myra is amazing.Myra is amazing.Myra is amazing.Myra is amazing.Myra is amazing.Myra is amazing.Myra is amazing.Myra is amazing.</p>
+							</div><!--/.col-md-3 -->
+							<!-- Testimonials -->
+							<section>
+								<div class="container">
+									<div class="row">
+										<div class="col-md-3 text-center">
+											<!-- Profile Pic-->
+											<img src="images/bobross.jpg" alt="doge" class="img-circle" id="myPhoto">
+											<!-- Testimonials -->
+											<div class="panel panel-default">
+												<div class="panel-body">
+													<p id="panel-bodyText">Myra is amazing.Myra is amazing.Myra is amazing.Myra is
+														amazing.Myra is amazing.Myra is amazing.Myra is amazing.Myra is amazing.Myra
+														is amazing.Myra is amazing.Myra is amazing.Myra is amazing.Myra is
+														amazing.</p>
+												</div>
 											</div>
-										</div>
-									</div><!--/.col-md-3 -->
-									<!-- Testimonials -->
-									<section>
-										<div class="container">
-											<div class="row">
-												<div class="col-md-3 text-center">
-													<!-- Profile Pic-->
-													<img src="images/bobross.jpg" alt="doge" class="img-circle" id="myPhoto">
-													<!-- Testimonials -->
-													<div class="panel panel-default">
-														<div class="panel-body">
-															<p id="panel-bodyText">Myra is amazing.Myra is amazing.Myra is amazing.Myra is amazing.Myra is amazing.Myra is amazing.Myra is amazing.Myra is amazing.Myra is amazing.Myra is amazing.Myra is amazing.Myra is amazing.Myra is amazing.</p>
+										</div><!--/.col-md-3 -->
+										<!-- Testimonials -->
+										<section>
+											<div class="container">
+												<div class="row">
+													<div class="col-md-3 text-center">
+														<!-- Profile Pic-->
+														<img src="images/bobross.jpg" alt="doge" class="img-circle" id="myPhoto">
+														<!-- Testimonials -->
+														<div class="panel panel-default">
+															<div class="panel-body">
+																<p id="panel-bodyText">Myra is amazing.Myra is amazing.Myra is
+																	amazing.Myra is amazing.Myra is amazing.Myra is amazing.Myra is
+																	amazing.Myra is amazing.Myra is amazing.Myra is amazing.Myra is
+																	amazing.Myra is amazing.Myra is amazing.</p>
+															</div>
 														</div>
-													</div>
-												</div><!--/.col-md-3 -->
-												<!-- Testimonials -->
-												<section>
-													<div class="container">
-														<div class="row">
-															<div class="col-md-3 text-center">
-																<!-- Profile Pic-->
-																<img src="images/bobross.jpg" alt="doge" class="img-circle" id="myPhoto">
-																<!-- Testimonials -->
-																<div class="panel panel-default">
-																	<div class="panel-body">
-																		<p id="panel-bodyText">Myra is amazing.Myra is amazing.Myra is amazing.Myra is amazing.Myra is amazing.Myra is amazing.Myra is amazing.Myra is amazing.Myra is amazing.Myra is amazing.Myra is amazing.Myra is amazing.Myra is amazing.</p>
+													</div><!--/.col-md-3 -->
+													<!-- Testimonials -->
+													<section>
+														<div class="container">
+															<div class="row">
+																<div class="col-md-3 text-center">
+																	<!-- Profile Pic-->
+																	<img src="images/bobross.jpg" alt="doge" class="img-circle"
+																		  id="myPhoto">
+																	<!-- Testimonials -->
+																	<div class="panel panel-default">
+																		<div class="panel-body">
+																			<p id="panel-bodyText">Myra is amazing.Myra is amazing.Myra is
+																				amazing.Myra is amazing.Myra is amazing.Myra is amazing.Myra
+																				is amazing.Myra is amazing.Myra is amazing.Myra is
+																				amazing.Myra is amazing.Myra is amazing.Myra is amazing.</p>
+																		</div>
+																	</div>
+																</div><!--/.col-md-3 -->
+																<!-- contact form -->
+																<div class="row">
+																	<h1 id="headers-ContactForm">Contact Bottomline Analytics here!</h1>
+																	<div class="col-md-6"></div>
+																	<div class="col-md-12 center-block">
+																		<form class="form-horizontal well" action="email.php">
+																			<div class="form-group">
+																				<label for="name">Name</label>
+																				<div class="input-group">
+																					<div class="input-group-addon">
+																						<i class="fa fa-user" aria-hidden="true"></i>
+																					</div>
+																					<input type="text" class="form-control" id="name"
+																							 name="name" placeholder="Name">
+																				</div>
+																			</div>
+																			<div class="form-group">
+																				<label for="email">Email address</label>
+																				<div class="input-group">
+																					<div class="input-group-addon">
+																						<i class="fa fa-envelope" aria-hidden="true"></i>
+																					</div>
+																					<input type="email" class="form-control" id="email"
+																							 name="email" placeholder="Email">
+																				</div>
+																			</div>
+																			<div class="form-group">
+																				<label for="subject">Subject</label>
+																				<div class="input-group">
+																					<div class="input-group-addon">
+																						<i class="fa fa-pencil" aria-hidden="true"></i>
+																					</div>
+																					<input type="text" class="form-control" id="subject"
+																							 name="subject" placeholder="Subject">
+																				</div>
+																			</div>
+																			<div class="form-group">
+																				<label for="message">Message</label>
+																				<div class="input-group">
+																					<div class="input-group-addon">
+																						<i class="fa fa-comment" aria-hidden="true"></i>
+																					</div>
+																					<textarea class="form-control" rows="5" id="message"
+																								 name="message"
+																								 placeholder="Message"></textarea>
+																				</div>
+																			</div>
+																			<button class="btn btn-success" type="submit"><i
+																					class="fa fa-paper-plane"></i> Send
+																			</button>
+																			<button class="btn btn-warning" type="reset"><i
+																					class="fa fa-ban"></i> Reset
+																			</button>
+																		</form>
 																	</div>
 																</div>
-															</div><!--/.col-md-3 -->
-															<!-- contact form -->
-															<div id="ContactForm" class="row">
-																<div class="col-md-6"></div>
-																<div class="col-md-12 center-block">
-																	<form class="form-horizontal well" action="email.php">
-																		<div class="form-group">
-																			<label for="name">Name</label>
-																			<div class="input-group">
-																				<div class="input-group-addon">
-																					<i class="fa fa-user" aria-hidden="true"></i>
-																				</div>
-																				<input type="text" class="form-control" id="name" name="name" placeholder="Name">
-																			</div>
-																		</div>
-																		<div class="form-group">
-																			<label for="email">Email address</label>
-																			<div class="input-group">
-																				<div class="input-group-addon">
-																					<i class="fa fa-envelope" aria-hidden="true"></i>
-																				</div>
-																				<input type="email" class="form-control" id="email" name="email" placeholder="Email">
-																			</div>
-																		</div>
-																		<div class="form-group">
-																			<label for="subject">Subject</label>
-																			<div class="input-group">
-																				<div class="input-group-addon">
-																					<i class="fa fa-pencil" aria-hidden="true"></i>
-																				</div>
-																				<input type="text" class="form-control" id="subject" name="subject" placeholder="Subject">
-																			</div>
-																		</div>
-																		<div class="form-group">
-																			<label for="message">Message</label>
-																			<div class="input-group">
-																				<div class="input-group-addon">
-																					<i class="fa fa-comment" aria-hidden="true"></i>
-																				</div>
-																				<textarea class="form-control" rows="5" id="message" name="message" placeholder="Message"></textarea>
-																			</div>
-																		</div>
-																		<button class="btn btn-success" type="submit"><i class="fa fa-paper-plane"></i> Send</button>
-																		<button class="btn btn-warning" type="reset"><i class="fa fa-ban"></i> Reset</button>
-																	</form>
+																<div class="row">
+																	<div class="col-xs-12">
+																		<div id="output-area"></div>
+																	</div>
 																</div>
-															</div>
-			<div class="row">
-				<div class="col-xs-12">
-					<div id="output-area"></div>
-				</div>
-			</div>
-			<footer id="bottomfooter" class="bg-info text-center">
-			<div class="container">
-				<div class="row">
-					<div id="bottomfooterText" class="col-md-4 col-md-offset-4">
-						Bottomline Analytics Myra Jordan, CRC, 505-440-8068
-					</div>
-				</div>
-			</div>
-		</footer>
+																<footer id="bottomfooter" class="bg-info text-center">
+																	<div class="container">
+																		<div class="row">
+																			<div id="bottomfooterText" class="col-md-4 col-md-offset-4">
+																				Bottomline Analytics Myra Jordan, CRC, 505-440-8068
+																			</div>
+																		</div>
+																	</div>
+																</footer>
 	</body>
 </html>
